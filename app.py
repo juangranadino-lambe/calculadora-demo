0061d00d86cbded7d7251761932f08057e60507c
juangranadino-lambe <juangranadino@gmail.com>
2026-03-12 18:53:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -106,10 +106,10 @@ with col_der:
     # Código HTML sin espacios a la izquierda para que Streamlit lo procese correctamente
     html_resultados = f"""
 <div style="background-color: #f0fdf4; border: 3px solid #16a34a; border-radius: 15px; padding: 25px 20px; text-align: center; height: 100%; box-shadow: 2px 4px 10px rgba(0,0,0,0.1);">
-    <p style="color: #166534; font-size: 1.2rem; font-weight: bold; margin: 0;">💶 COSTE UNITARIO</p>
+    <p style="color: #166534a; font-size: 1.2rem; font-weight: bold; margin: 0;">💶 COSTE UNITARIO</p>
     <h1 style="color: #16a34a; font-size: 4rem; font-weight: 900; margin: 0; line-height: 1.1;">{coste_unitario:,.3f} €</h1>
     <hr style="border-color: #16a34a; margin: 25px 0;">
-    <p style="color: #166534; font-size: 1.2rem; font-weight: bold; margin: 0;">📚 TOTAL EDICIÓN</p>
+    <p style="color: #166534a; font-size: 1.2rem; font-weight: bold; margin: 0;">📚 TOTAL EDICIÓN</p>
     <h2 style="color: #16a34a; font-size: 2.5rem; font-weight: 800; margin: 0;">{total_edicion:,.2f} €</h2>
     <div style="margin-top: 30px; text-align: left; background-color: #ffffff; padding: 15px; border-radius: 10px; border: 5px solid #22c55e;">
         <p style="margin: 8px 0; font-size: 1.1rem; color: #374151;">🏭 <b>Total Producción:</b> {total_produccion:,.2f} €</p>
